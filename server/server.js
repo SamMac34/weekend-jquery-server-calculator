719b6f855b33f57d7f021d5751a7ecdc5a69808b
Sam McCauley <sammccauley34@gmail.com>
2023-06-15 20:59:21 -0500

    Added base express variables and functions to server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,26 @@
 const express =require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = 5000;
+
+app.use(express.static('server/public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log('listening on port:', 5000);
+});
